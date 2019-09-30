a9e1068a297c4017050ddc4024b27b3e50f30cc9
Hakeem-star <53916666+Hakeem-star@users.noreply.github.com>
2019-09-30 20:36:50 +0100

    Update
    Added more states for General and Celeb switching. Need to implement API and improve styling

--- src/App.js
+++ src/App.js
@@ -1,49 +1,80 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function PageTitle(props){
+function PageTitle(props) {
 
   return (
-    <h1>{props.title} Anagram</h1>
+    <div className="headerPortion">
+      <h1>{props.title + " Anagram Finder"}</h1>
+    </div>
   )
 }
 
-function SearchSelector (props){
+function SearchSelector(props) {
 
-  return(
-        <div>
-            <div>
-                <select name="anagramQuerySelect" id="anagramQuerySelect">
-                    <option selected="selected" className="celebrities" value="Celebrities">Celebrities</option>
-                    <option className="general" value="General">General</option>
-
-                </select>
-            </div>
-        </div>
+  return (
+    <div>
+      <div >
+        <select className="selector" onChange={props.handleClick} name="anagramQuerySelect" id="anagramQuerySelect">
+          <option className="celebrities" value="Celebrities">Celebrities</option>
+          <option className="general" value="General">General</option>
+        </select>
+      </div>
+    </div>
   )
-      }
+}
 
-      function SearchInput(){
-return (
-<div>
-  <input type="text" placeholder = "Thanksmo,TRUMCDONALD,parsi/hl itno"></input>
-  <input type="button" value="Search"/>
-  </div>
-)
-      }
+
+function SearchInput(props) {
+  let name;
+  props.name === "General" ? name = "Value" : name = "Name";
+
+  return (
+    <div className="searchInput">
+      <div className="inputName">{"Enter " + name}</div>
+      <input onChange={props.searchTerm} type="text" placeholder="Thanksmo,TRUMCDONALD,parsi/hl itno"></input>
+      <input type="button" value="Search" />
+    </div>
+  )
+}
+
 
 class App extends React.Component {
 
-  render(){
-  return (
-    <div>
-    <PageTitle/>
-    <SearchSelector />
-    <SearchInput />
-    </div>
-  );
+  constructor(props) {
+    super(props)
+    this.state = {
+      anagramType: PageTitle.defaultProps.title,
+      SearchInputName: PageTitle.defaultProps.title,
+      searchTerm: "",
+    }
   }
+
+  searchSelected(e) {
+    //console.dir(e.target)
+    this.setState({ anagramType: e.target.value, SearchInputName: e.target.value });
+  }
+
+  searchTerm(e) {
+    //console.log(e.target.value)
+    this.setState({ searchTerm: e.target.value });
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <PageTitle title={this.state.anagramType} />
+        <SearchSelector handleClick={(e) => this.searchSelected(e)} />
+        <SearchInput name={this.state.anagramType} searchTerm={(e) => this.searchTerm(e)} />
+      </div>
+    );
+  }
+}
+
+
+PageTitle.defaultProps = {
+  title: "Celebrity",
+  SearchInputName: "Name",
 }
 
 export default App;
