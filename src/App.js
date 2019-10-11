9000b05eb7f47254e8ccc57cf7dcbffcdd34f0b5
Hakeem-star <53916666+Hakeem-star@users.noreply.github.com>
2019-10-11 01:23:53 +0100

    Updated SearchBar
    Updated styling of search bar. Need to work on input values when anagram type is changed from Celeb to General

--- src/App.js
+++ src/App.js
@@ -1,179 +1,7 @@
 import React from 'react';
+import celebAnagramFinder from './celebAnagramFinderAPICall';
 import './App.css';
 
-
-async function celebAnagramFinder(value) {
-
-
-  function cleanInputValue(value) {
-    //write input value to anagram variable
-    //Check if there are multiple ANAGRAMS and split into array
-    if (value.toUpperCase().includes(",")) {
-      //Create array from comma seperated anagram and remove spaces
-      value = value.toUpperCase().replace(/\s/g, '').split(',');
-      //Remove any non alphabet values
-      value.forEach(function (val, index) {
-        value[index] = val.replace(/[^A-Z]/g, "");
-      })
-      // console.log(anagram)
-    } else {
-      //If there is only one ANAGRAM value
-      //Place Anagram into Array
-      value = [value.toUpperCase().replace(/\s/g, '')]
-      //Remove any non alphabet values
-      value.forEach(function (val, index) {
-        value[index] = val.replace(/[^A-Z]/g, "");
-      })
-    };
-
-    return value
-  }
-
-  let anagram = cleanInputValue(value);
-
-  //console.log(anagram)
-
-  //Bypass CORS if needed. Replace fetch url with proxyUrl + targetUrl
-  let proxyUrl = 'https://cors-anywhere.herokuapp.com/'
-  let targetUrl = 'https://celebritybucks.com/developers/export/JSON'
-
-  let matchRateResult = {};
-
-  //Fetch the json of celebs
-  let resFetch = await fetch(proxyUrl + targetUrl);
-
-  let celebsFromApi = await resFetch.json();
-
-
-  //covnert result to JSOn
-
-  function apiChange() {
-
-    //If it fails, log message and quit
-    if (resFetch.status !== 200) {
-      console.log('Looks like there was a problem. Status Code: ' +
-        resFetch.status);
-      return;
-    }
-    // STRUCTURE---- celebsFromApi = {CelebrityValues: [{ name: "Serena Williams" },{ name: "Kacey Musgraves" }, { name: "Ivana Milicevic" } ] };
-    // console.log("Celebs from API", celebsFromApi.CelebrityValues)
-
-    //Example Anagrams to search for
-    /* var anagram = ["TRUMCDONALD", "SHAVERSINB", "SHAVERSINC", "SHAVERSINM", "SHAVERSINP", "HASTYMAREA", "HASTYMAREE", "HASTYMAREI",
-         "HASTYMAREL", "HASTYMAREN", "HASTYMAREO", "HASTYMARER", "HASTYMARES", "HASTYMARET", "HASTYMAREU", "SLIMSWINEAREAA",
-         "SLIMSWINEAREAE", "SLIMSWINEAREAI", "SLIMSWINEAREAL", "SLIMSWINEAREAN", "SLIMSWINEAREAO", "SLIMSWINEAREAR", "SLIMSWINEAREAS",
-         "SLIMSWINEAREAT", "SLIMSWINEAREAU", "ROTTENGROANA", "ROTTENGROANE", "ROTTENGROANI", "ROTTENGROANL", "ROTTENGROANN", "ROTTENGROANO",
-         "ROTTENGROANR", "ROTTENGROANS", "ROTTENGROANT", "ROTTENGROANU", "GRIMERTABOOB", "GRIMERTABOOC", "GRIMERTABOOM", "GRIMERTABOOP", "PUTINKARMAF",
-         "PUTINKARMAH", "PUTINKARMAV", "PUTINKARMAW", "PUTINKARMAY", "SLOTHMENWAILA", "SLOTHMENWAILE", "SLOTHMENWAILI", "SLOTHMENWAILL", "SLOTHMENWAILN",
-         "SLOTHMENWAILO", "SLOTHMENWAILR", "SLOTHMENWAILS", "SLOTHMENWAILT", "SLOTHMENWAILU", "BENLAIDLAWB", "BENLAIDLAWC", "BENLAIDLAWM", "BENLAIDLAWP",
-         "ONEMUTTISLANDA", "ONEMUTTISLANDE", "ONEMUTTISLANDI", "ONEMUTTISLANDL", "ONEMUTTISLANDN", "ONEMUTTISLANDO", "ONEMUTTISLANDR", "ONEMUTTISLANDS",
-         "ONEMUTTISLANDT", "ONEMUTTISLANDU", "JEDIMATHSA", "JEDIMATHSE", "JEDIMATHSI", "JEDIMATHSL", "JEDIMATHSN", "JEDIMATHSO", "JEDIMATHSR", "JEDIMATHSS",
-         "JEDIMATHST", "JEDIMATHSU"];
-*/
-    //Go through the anagram array  
-    for (let k_counter = 0; k_counter < anagram.length; k_counter++) {
-
-      //Split the anagram into an array of individual letters
-      let splitAnagramToLetters = anagram[k_counter].split("");
-      //console.log(splitAnagramToLetters)
-      //[S,H,A,V,E,R,S,I,N]
-
-      //Count the occurances of letters within the current anagram
-      let anagramLetterCount = {};
-      //make an array of each letter and iterate through
-      splitAnagramToLetters.forEach(function (ana, ind, initialArray) {
-        //Create an object that uses the letter as a key
-        //Then with a filter, it iterates through the array containing all the ANAGRAM letters to compare the key letter
-        //Then uses the length of the filtered result as the amount of occurances 
-        anagramLetterCount[ana] = initialArray.filter(function (filteredAna) {
-          return filteredAna === ana;
-        }).length
-      });
-
-
-
-
-      //Iterate that against a loop going through the resulting array of celebs - 
-      //For each resulting celeb, check the matching letters
-      for (let i_counter = 0; i_counter < celebsFromApi.CelebrityValues.length; i_counter++) {
-        let celebNameCleanedUp = celebsFromApi.CelebrityValues[i_counter].name.toUpperCase().replace(" ", "").replace(/[^A-Z]/g, "");
-        //If the length of the word in the anagram array is the same as the name from the api call (with spaces removed)
-        if (anagram[k_counter].length === celebNameCleanedUp.length) {
-
-          //Make an empty object
-          let matchedLetters = {};
-
-          //Iterate through the splitAnagramToLetters array and count letter matches with celeb name
-          for (let l_counter = 0; l_counter < splitAnagramToLetters.length; l_counter++) {
-
-            //If the name from the celebs api array contains the letter from the splitAnagramToLetters array
-            if (celebNameCleanedUp.includes(splitAnagramToLetters[l_counter])) {
-
-              //If the ind object does not contain the current letter from the splitAnagramToLetters array (because that letter has not been checked yet)
-              if (!matchedLetters[splitAnagramToLetters[l_counter]]) {
-                //Find the amount of times the current letter from the splitAnagramToLetters array shows up in the celebsFromApi name
-                let anagramLetterInCelebLetterCount = celebNameCleanedUp.split("").filter((data) => data === splitAnagramToLetters[l_counter]).length;
-
-                // let lettersInAnagramCount = splitAnagramToLetters.filter((data) => data === splitAnagramToLetters[l_counter]).length;
-                //If the amount of times this letter is found is larger than the amount of times it's in the anagram, then we need to break and leave the loop and move on to the next anagram
-
-                if (anagramLetterInCelebLetterCount > (anagramLetterCount[splitAnagramToLetters[l_counter]] + 1)) {
-                  //debugger;
-                  break;
-                }
-                else {
-                  matchedLetters[splitAnagramToLetters[l_counter]] = anagramLetterInCelebLetterCount;
-                  //{A:1,B:3}
-                }
-                //Add the splitAnagramToLetters letter into the ind object along with the amount of times the current showed up
-              }
-
-            }
-          }
-
-
-          //Compare the 2 objects and count the amount of matches
-          //console.log(anagramLetterCount, ind)
-          let count = 0;
-          let totalCount = 0;
-          //Iterate through the keys of the anagramLetterCount object which keeps a record of the occurance of letters in the celeb name
-          //The count variable keeps a count of the matches
-          Object.keys(anagramLetterCount).forEach(function (indVal) {
-            if (matchedLetters[indVal] === anagramLetterCount[indVal]) { //console.log(indVal, ind[indVal],anagramLetterCount[indVal] )
-              count++;
-            }
-          });
-
-          //totalCount gets the total count of matches then converts this into a percentage value based on the amount of matches
-          totalCount = ((count / Object.keys(anagramLetterCount).length) * 100).toFixed();
-
-
-          //Control the threshold for what is shown in the results
-          if (totalCount >= 80) {
-            if (!matchRateResult[anagram[k_counter]]) {
-              //Create an empty array is it doesn't
-              matchRateResult[anagram[k_counter]] = []
-
-            }
-            //Push the resulting Celeb and match rate to the obj object
-            //obj.*anagramName* = [{Celeb Name: 90%},{Other Celeb Name: 90%}]
-            matchRateResult[anagram[k_counter]].push({ [celebsFromApi.CelebrityValues[i_counter].name]: totalCount + "%" });
-          }
-        }
-      }
-    }
-
-    console.log(matchRateResult)
-
-    return new Promise(function (resolve, reject) {
-      resolve(matchRateResult);
-      //throw new Error("Something went badly wrong!");
-    })
-  };
-
-  return await apiChange();
-  // return  matchRateResult;
-};
 
 function NothingFound(props) {
   return (
@@ -182,40 +10,107 @@ function NothingFound(props) {
 }
 
 
-function ResultsFound(props) {
+class ResultsFound extends React.Component {
 
-  let rows = (Array(Object.keys(props.results).length).fill("").map((value, ind) => {
-    let anagramName = Object.keys(props.results)[ind]; //Thanksmo       
-    let val = props.results[anagramName][0] //return the array associated with the anagram
-    /*0: {Tom Hanks: "100%"} */
-    let celebName = Object.keys(val)[0]; //Create an array based off the keys in the returned object. 
-    //"Tom Hanks"
-    let innerCurrent = val[celebName]
-    return (
-      <tr key={anagramName + celebName}>
-        <td>{anagramName}</td>
-        <td>{celebName}</td>
-        <td>{innerCurrent}</td>
-      </tr>
-    )
-  }));
+  constructor(props) {
+    super(props)
+    this.state = {
+      currentResultsPage: 0,
+      maxPages: 1,
+    }
+  }
 
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th>Anagram</th>
-          <th>Name</th>
-          <th>Match Rate</th>
+
+
+  //Seperate the results into chunks, so we can have different pages for the results
+  chunk(arr, chunckSize) {
+    // debugger;
+    let newChunk = [];
+    let j = 0;
+    for (let i = 0; i < arr.length; i += chunckSize) {
+      !i ? j = 0 : j += chunckSize;
+      newChunk.push(arr.slice(j, j + chunckSize))
+    }
+
+    return newChunk;
+  }
+
+  navigateResults(e) {
+    if (e.target.textContent === " » ") {
+      if (this.state.currentResultsPage < this.state.maxPages-1) {
+        this.setState({ currentResultsPage: this.state.currentResultsPage + 1 })
+      }
+    } else if (e.target.textContent === " « "){
+      if (this.state.currentResultsPage > 0) {
+        this.setState({ currentResultsPage: this.state.currentResultsPage - 1 })
+      }
+    }
+    console.dir(e.target.textContent);
+  }
+  componentDidMount() {
+    //console.log( this.chunk(Object.keys(this.props.results),10));
+    this.setState({ maxPages: this.chunk(Object.keys(this.props.results),10).length });
+  }
+
+  render() {
+
+    let rows = (Array(Object.keys(this.props.results).length).fill("").map((value, ind) => {
+
+      let anagramName = Object.keys(this.props.results)[ind]; //Thanksmo       
+      let val = this.props.results[anagramName][0] //return the array associated with the anagram
+      /*0: {Tom Hanks: "100%"} */
+      let celebName = Object.keys(val)[0]; //Create an array based off the keys in the returned object. 
+      //"Tom Hanks"
+      let innerCurrent = val[celebName]
+      return (
+        <tr key={anagramName + celebName}>
+          <td>{anagramName}</td>
+          <td>{celebName}</td>
+          <td>{innerCurrent}</td>
         </tr>
-      </thead>
-      <tbody>
-        {rows}
-      </tbody>
-    </table>
-  )
+      )
+
+    }));
+    
+
+    let resultsCounter = this.state.currentResultsPage > 0 ? 
+    (this.chunk(rows, 10)[this.state.currentResultsPage].length) + (this.state.currentResultsPage *10) :
+    this.chunk(rows, 10)[this.state.currentResultsPage].length
+
+    return (
+      <div className="container">
+        <table>
+          <thead>
+            <tr>
+              <th>Anagram</th>
+              <th>Name</th>
+              <th>Match Rate</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.chunk(rows, 10)[this.state.currentResultsPage]}
+          </tbody>
+        </table>
+        <ResultsPageNumber prevResults={(e) => this.navigateResults(e)}
+          current={resultsCounter}
+          nextResults={(e) => this.navigateResults(e)} maximum={rows.length} />
+      </div>
+    )
+  }
 }
 
+
+
+function ResultsPageNumber(props) {
+
+  return (
+    <div className="ResultsPageNumber">
+      <p className="next" onClick={props.prevResults}> « </p>
+      <p>Showing {props.current} of {props.maximum}</p>
+      <p className="previous" onClick={props.nextResults}> » </p>
+    </div>
+  )
+}
 
 function ResultDivContain(props) {
 
@@ -224,11 +119,12 @@ function ResultDivContain(props) {
   if (Object.keys(props.results).length === 0) {
     results = <NothingFound />;
   } else {
-    results = <ResultsFound results={props.results} />
+    results = <ResultsFound
+      page={props.resultsPage} results={props.results} />
   }
 
   return (
-    <div className="resultDivContainer">
+    <div className="container">
       {results}
     </div>
   )
@@ -266,7 +162,13 @@ function SearchInput(props) {
 
   return (
     <div className="searchInput">
-      <input className="searchTextInput" onKeyPress={props.submitted} onChange={props.searchTerm} type="text" placeholder={placeholderText}></input>
+      <input
+        className="searchTextInput"
+        onKeyPress={props.submitted}
+        onChange={props.searchTerm}
+        type="text"
+        defaultValue={placeholderText}>
+      </input>
       <input onClick={props.submitted} type="button" value="Search" />
     </div>
   )
@@ -282,6 +184,8 @@ class App extends React.Component {
       SearchInputName: PageTitle.defaultProps.title,
       searchTerm: "",
       results: "",
+      resultsIndex: 0,
+
     }
   }
 
@@ -305,23 +209,27 @@ class App extends React.Component {
     let rows;
     if (typeof (this.state.results) === "string") {
       rows = [];
-    } else if (Object.keys(this.state.results).length === 0) {
-      rows = <NothingFound />;
-    } else { rows = <ResultDivContain results={this.state.results} /> }
+    } else {
+      rows = <ResultDivContain
+        results={this.state.results} prevResults={this.props.gotoPrevResults}
+        nextResults={this.props.gotoNextResults} current={this.state.resultsIndex} />
+    }
+
+
     return (
       <div>
 
-      <div>
-        <PageTitle title={this.state.anagramType} />
-
-        <div className="inputSubmitContainer">
-          <SearchSelector handleClick={(e) => this.searchSelected(e)} />
-          <SearchInput placeholder={this.state.anagramType} submitted={(e) => this.searchSubmitted(e)} />
-        </div>
         <div>
-          {rows}
+          <PageTitle title={this.state.anagramType} />
+
+          <div className="inputSubmitContainer">
+            <SearchSelector handleClick={(e) => this.searchSelected(e)} />
+            <SearchInput placeholder={this.state.anagramType} submitted={(e) => this.searchSubmitted(e)} />
+          </div>
+          <div>
+            {rows}
+          </div>
         </div>
-      </div>
       </div>
     );
   }
---
+++ src/celebAnagramFinderAPICall.js
@@ -0,0 +1,172 @@
+export default async function celebAnagramFinder(value) {
+
+
+    function cleanInputValue(value) {
+      //write input value to anagram variable
+      //Check if there are multiple ANAGRAMS and split into array
+      if (value.toUpperCase().includes(",")) {
+        //Create array from comma seperated anagram and remove spaces
+        value = value.toUpperCase().replace(/\s/g, '').split(',');
+        //Remove any non alphabet values
+        value.forEach(function (val, index) {
+          value[index] = val.replace(/[^A-Z]/g, "");
+        })
+        // console.log(anagram)
+      } else {
+        //If there is only one ANAGRAM value
+        //Place Anagram into Array
+        value = [value.toUpperCase().replace(/\s/g, '')]
+        //Remove any non alphabet values
+        value.forEach(function (val, index) {
+          value[index] = val.replace(/[^A-Z]/g, "");
+        })
+      };
+  
+      return value
+    }
+  
+    let anagram = cleanInputValue(value);
+  
+    //console.log(anagram)
+  
+    //Bypass CORS if needed. Replace fetch url with proxyUrl + targetUrl
+    let proxyUrl = 'https://cors-anywhere.herokuapp.com/'
+    let targetUrl = 'https://celebritybucks.com/developers/export/JSON'
+  
+    let matchRateResult = {};
+  
+    //Fetch the json of celebs
+    let resFetch = await fetch(proxyUrl + targetUrl);
+  
+    let celebsFromApi = await resFetch.json();
+  
+  
+    //covnert result to JSOn
+  
+    function apiChange() {
+  
+      //If it fails, log message and quit
+      if (resFetch.status !== 200) {
+        console.log('Looks like there was a problem. Status Code: ' +
+          resFetch.status);
+        return;
+      }
+      // STRUCTURE---- celebsFromApi = {CelebrityValues: [{ name: "Serena Williams" },{ name: "Kacey Musgraves" }, { name: "Ivana Milicevic" } ] };
+      // console.log("Celebs from API", celebsFromApi.CelebrityValues)
+  
+      //Example Anagrams to search for
+      /* var anagram = ["TRUMCDONALD", "SHAVERSINB", "SHAVERSINC", "SHAVERSINM", "SHAVERSINP", "HASTYMAREA", "HASTYMAREE", "HASTYMAREI",
+           "HASTYMAREL", "HASTYMAREN", "HASTYMAREO", "HASTYMARER", "HASTYMARES", "HASTYMARET", "HASTYMAREU", "SLIMSWINEAREAA",
+           "SLIMSWINEAREAE", "SLIMSWINEAREAI", "SLIMSWINEAREAL", "SLIMSWINEAREAN", "SLIMSWINEAREAO", "SLIMSWINEAREAR", "SLIMSWINEAREAS",
+           "SLIMSWINEAREAT", "SLIMSWINEAREAU", "ROTTENGROANA", "ROTTENGROANE", "ROTTENGROANI", "ROTTENGROANL", "ROTTENGROANN", "ROTTENGROANO",
+           "ROTTENGROANR", "ROTTENGROANS", "ROTTENGROANT", "ROTTENGROANU", "GRIMERTABOOB", "GRIMERTABOOC", "GRIMERTABOOM", "GRIMERTABOOP", "PUTINKARMAF",
+           "PUTINKARMAH", "PUTINKARMAV", "PUTINKARMAW", "PUTINKARMAY", "SLOTHMENWAILA", "SLOTHMENWAILE", "SLOTHMENWAILI", "SLOTHMENWAILL", "SLOTHMENWAILN",
+           "SLOTHMENWAILO", "SLOTHMENWAILR", "SLOTHMENWAILS", "SLOTHMENWAILT", "SLOTHMENWAILU", "BENLAIDLAWB", "BENLAIDLAWC", "BENLAIDLAWM", "BENLAIDLAWP",
+           "ONEMUTTISLANDA", "ONEMUTTISLANDE", "ONEMUTTISLANDI", "ONEMUTTISLANDL", "ONEMUTTISLANDN", "ONEMUTTISLANDO", "ONEMUTTISLANDR", "ONEMUTTISLANDS",
+           "ONEMUTTISLANDT", "ONEMUTTISLANDU", "JEDIMATHSA", "JEDIMATHSE", "JEDIMATHSI", "JEDIMATHSL", "JEDIMATHSN", "JEDIMATHSO", "JEDIMATHSR", "JEDIMATHSS",
+           "JEDIMATHST", "JEDIMATHSU"];
+  */
+      //Go through the anagram array  
+      for (let k_counter = 0; k_counter < anagram.length; k_counter++) {
+  
+        //Split the anagram into an array of individual letters
+        let splitAnagramToLetters = anagram[k_counter].split("");
+        //console.log(splitAnagramToLetters)
+        //[S,H,A,V,E,R,S,I,N]
+  
+        //Count the occurances of letters within the current anagram
+        let anagramLetterCount = {};
+        //make an array of each letter and iterate through
+        splitAnagramToLetters.forEach(function (ana, ind, initialArray) {
+          //Create an object that uses the letter as a key
+          //Then with a filter, it iterates through the array containing all the ANAGRAM letters to compare the key letter
+          //Then uses the length of the filtered result as the amount of occurances 
+          anagramLetterCount[ana] = initialArray.filter(function (filteredAna) {
+            return filteredAna === ana;
+          }).length
+        });
+  
+  
+  
+  
+        //Iterate that against a loop going through the resulting array of celebs - 
+        //For each resulting celeb, check the matching letters
+        for (let i_counter = 0; i_counter < celebsFromApi.CelebrityValues.length; i_counter++) {
+          let celebNameCleanedUp = celebsFromApi.CelebrityValues[i_counter].name.toUpperCase().replace(" ", "").replace(/[^A-Z]/g, "");
+          //If the length of the word in the anagram array is the same as the name from the api call (with spaces removed)
+          if (anagram[k_counter].length === celebNameCleanedUp.length) {
+  
+            //Make an empty object
+            let matchedLetters = {};
+  
+            //Iterate through the splitAnagramToLetters array and count letter matches with celeb name
+            for (let l_counter = 0; l_counter < splitAnagramToLetters.length; l_counter++) {
+  
+              //If the name from the celebs api array contains the letter from the splitAnagramToLetters array
+              if (celebNameCleanedUp.includes(splitAnagramToLetters[l_counter])) {
+  
+                //If the ind object does not contain the current letter from the splitAnagramToLetters array (because that letter has not been checked yet)
+                if (!matchedLetters[splitAnagramToLetters[l_counter]]) {
+                  //Find the amount of times the current letter from the splitAnagramToLetters array shows up in the celebsFromApi name
+                  let anagramLetterInCelebLetterCount = celebNameCleanedUp.split("").filter((data) => data === splitAnagramToLetters[l_counter]).length;
+  
+                  // let lettersInAnagramCount = splitAnagramToLetters.filter((data) => data === splitAnagramToLetters[l_counter]).length;
+                  //If the amount of times this letter is found is larger than the amount of times it's in the anagram, then we need to break and leave the loop and move on to the next anagram
+  
+                  if (anagramLetterInCelebLetterCount > (anagramLetterCount[splitAnagramToLetters[l_counter]] + 1)) {
+                    //debugger;
+                    break;
+                  }
+                  else {
+                    matchedLetters[splitAnagramToLetters[l_counter]] = anagramLetterInCelebLetterCount;
+                    //{A:1,B:3}
+                  }
+                  //Add the splitAnagramToLetters letter into the ind object along with the amount of times the current showed up
+                }
+  
+              }
+            }
+  
+  
+            //Compare the 2 objects and count the amount of matches
+            //console.log(anagramLetterCount, ind)
+            let count = 0;
+            let totalCount = 0;
+            //Iterate through the keys of the anagramLetterCount object which keeps a record of the occurance of letters in the celeb name
+            //The count variable keeps a count of the matches
+            Object.keys(anagramLetterCount).forEach(function (indVal) {
+              if (matchedLetters[indVal] === anagramLetterCount[indVal]) { //console.log(indVal, ind[indVal],anagramLetterCount[indVal] )
+                count++;
+              }
+            });
+  
+            //totalCount gets the total count of matches then converts this into a percentage value based on the amount of matches
+            totalCount = ((count / Object.keys(anagramLetterCount).length) * 100).toFixed();
+  
+  
+            //Control the threshold for what is shown in the results
+            if (totalCount >= 80) {
+              if (!matchRateResult[anagram[k_counter]]) {
+                //Create an empty array is it doesn't
+                matchRateResult[anagram[k_counter]] = []
+  
+              }
+              //Push the resulting Celeb and match rate to the obj object
+              //obj.*anagramName* = [{Celeb Name: 90%},{Other Celeb Name: 90%}]
+              matchRateResult[anagram[k_counter]].push({ [celebsFromApi.CelebrityValues[i_counter].name]: totalCount + "%" });
+            }
+          }
+        }
+      }
+  
+      console.log(matchRateResult)
+  
+      return new Promise(function (resolve, reject) {
+        resolve(matchRateResult);
+        //throw new Error("Something went badly wrong!");
+      })
+    };
+  
+    return await apiChange();
+    // return  matchRateResult;
+  };
