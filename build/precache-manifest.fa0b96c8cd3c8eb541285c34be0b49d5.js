2a2e998f66f56ada94bc55794322a9796c1839dd
Hakeem-star <53916666+Hakeem-star@users.noreply.github.com>
2019-11-04 11:42:00 +0000

    Included build folder

---
+++ build/precache-manifest.fa0b96c8cd3c8eb541285c34be0b49d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b94a7d405af56e17278aeacc8d436659",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4b4b73ebace1e36d151",
+    "url": "/static/css/main.bde97ec6.chunk.css"
+  },
+  {
+    "revision": "2264eba4d8dcede36690",
+    "url": "/static/js/2.7df4fa48.chunk.js"
+  },
+  {
+    "revision": "b4b4b73ebace1e36d151",
+    "url": "/static/js/main.41cad2e1.chunk.js"
+  },
+  {
+    "revision": "88937bb6aaf89ace47ce",
+    "url": "/static/js/runtime-main.3f7e2f43.js"
+  }
+]);
