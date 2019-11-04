9b2b6b082625f59934e3a7eb2af2bc4b74d06a79
Hakeem-star <53916666+Hakeem-star@users.noreply.github.com>
2019-11-04 12:31:57 +0000

    Updates

---
+++ precache-manifest.4429d6e0e51d4f09ffff175c6ed3c5ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5401dc8ad89f88fab340e10c2dc1658d",
+    "url": "/React-celeb-anagram-finder/index.html"
+  },
+  {
+    "revision": "802d99e1cf0e2e35fcca",
+    "url": "/React-celeb-anagram-finder/static/css/main.bde97ec6.chunk.css"
+  },
+  {
+    "revision": "2264eba4d8dcede36690",
+    "url": "/React-celeb-anagram-finder/static/js/2.7df4fa48.chunk.js"
+  },
+  {
+    "revision": "802d99e1cf0e2e35fcca",
+    "url": "/React-celeb-anagram-finder/static/js/main.17fa198b.chunk.js"
+  },
+  {
+    "revision": "b09ab01ec25e87c1b5e0",
+    "url": "/React-celeb-anagram-finder/static/js/runtime-main.b56b21e0.js"
+  }
+]);
